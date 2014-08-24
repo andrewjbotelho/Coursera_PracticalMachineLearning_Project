864ad544b95c6ee44d095690939b513efcaacf78
Andrew Botelho <andrewjbotelho@yahoo.com>
2014-08-24 03:46:24 -0400

    Update project_script.R

--- project_script.R
+++ project_script.R
@@ -6,14 +6,16 @@ testing <- read.csv("pml-testing.csv", header=TRUE)
 
 # Split the training set into another training and testing set
 inTrain <- createDataPartition(y=training$classe, p=0.7, list=FALSE)
-subTraining <- training[inTrain,]
-subTesting <- training[-inTrain,]
+subTraining <- training[inTrain,] # we will be training the model on this dataset
+subTesting <- training[-inTrain,] # we will test the model on this dataset
 
+# View plots of some of the variables
 qplot(roll_belt, pitch_belt, colour=classe, data=subTraining)
 qplot(accel_belt_x, accel_belt_y, colour=classe, data=subTraining)
 qplot(magnet_belt_x, magnet_belt_y, colour=classe, data=subTraining)
 qplot(magnet_arm_x, magnet_arm_y, colour=classe, data=subTraining)
 
+# Create the Random Forest Model
 modFit <- randomForest(classe ~ roll_belt + pitch_belt + yaw_belt + total_accel_belt +
                   accel_belt_x + accel_belt_y + accel_belt_z +
                   magnet_belt_x + magnet_belt_y + magnet_belt_z +
@@ -24,4 +26,5 @@ modFit <- randomForest(classe ~ roll_belt + pitch_belt + yaw_belt + total_accel_
                   gyros_arm_x + gyros_arm_y + gyros_arm_z +
                   gyros_belt_x + gyros_belt_y + gyros_belt_z, data=subTraining)
 
+# View the confusion matrix of how the model did predicting against the subTesting dataset
 confusionMatrix(subTesting$classe, predict(modFit, subTesting))
